564a7aa60bd591c6360369858756009cf7d04a21
Ramses Martinez <ramses.mtz96@gmail.com>
2022-11-07 21:25:13 -0600

    Se agregan estilos de imagen

---
+++ src/components/AvatarComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- <img class="img-avatar" alt="Vue logo" src="avatarImg" />
+    <img class="img-avatar" alt="Vue logo" :src="'avatarImg'" /> -->
+    <img class="img-avatar" alt="Vue logo" :src="avatarImg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvatarComponent',
+  computed: {
+    avatarImg() {
+      return this.$store.state.userAvatar;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.img-avatar {
+  width: 200px;
+  margin-bottom: 10px;
+  border-radius: 50%;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,13 @@ export default new Vuex.Store({
   state: {
     nombre: 'Marcos Martinez Jorge Rodrigo',
     edad: 18,
+    userAvatar: 'https://st2.depositphotos.com/2498595/6610/v/600/depositphotos_66106945-stock-illustration-business-avatar-ico.jpg',
+    imagenesDisponibles: [
+      'https://st2.depositphotos.com/2498595/6610/v/600/depositphotos_66106945-stock-illustration-business-avatar-ico.jpg',
+      'https://www.crushpixel.com/big-static19/preview4/man-avatar-icon-flat-style-3746672.jpg',
+      'https://previews.123rf.com/images/yupiramos/yupiramos1707/yupiramos170715915/82083101-avatar-hombre-icono-sobre-fondo-blanco-ilustraci%C3%B3n-vectorial-de-dise%C3%B1o-colorido.jpg',
+      'https://img.favpng.com/15/19/18/computer-icons-ninja-favicon-iconfinder-avatar-png-favpng-Jku9r1TGGY4Ha6BLUXz1eL3DQ.jpg',
+    ],
   },
   mutations: {
     addAgeMutation(state, age) {
