f985cd9f9f54da109d4cdb70f7181e2a8a597164
Ramses Martinez <ramses.mtz96@gmail.com>
2022-11-08 15:23:26 -0600

    cambios componentes

--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,31 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    nombre: 'Marcos Martinez Jorge Rodrigo',
+    nombre: 'Ramses Martinez',
     edad: 18,
-    userAvatar: 'https://st2.depositphotos.com/2498595/6610/v/600/depositphotos_66106945-stock-illustration-business-avatar-ico.jpg',
-    imagenesDisponibles: [
+    userAvatar:
       'https://st2.depositphotos.com/2498595/6610/v/600/depositphotos_66106945-stock-illustration-business-avatar-ico.jpg',
-      'https://www.crushpixel.com/big-static19/preview4/man-avatar-icon-flat-style-3746672.jpg',
-      'https://previews.123rf.com/images/yupiramos/yupiramos1707/yupiramos170715915/82083101-avatar-hombre-icono-sobre-fondo-blanco-ilustraci%C3%B3n-vectorial-de-dise%C3%B1o-colorido.jpg',
-      'https://img.favpng.com/15/19/18/computer-icons-ninja-favicon-iconfinder-avatar-png-favpng-Jku9r1TGGY4Ha6BLUXz1eL3DQ.jpg',
+    imagenesDisponibles: [
+      {
+        id: 12,
+        title: 'avatar 1',
+        url: 'https://e7.pngegg.com/pngimages/1012/711/png-clipart-pokxe9mon-go-video-game-ico-icon-lovely-elf-avatar-love-game-thumbnail.png',
+      },
+      {
+        id: 20,
+        title: 'avatar 2',
+        url: 'https://st2.depositphotos.com/2498595/6610/v/600/depositphotos_66106945-stock-illustration-business-avatar-ico.jpg',
+      },
+      {
+        id: 31,
+        title: 'avatar 3',
+        url: 'https://previews.123rf.com/images/yupiramos/yupiramos1707/yupiramos170715915/82083101-avatar-hombre-icono-sobre-fondo-blanco-ilustraci%C3%B3n-vectorial-de-dise%C3%B1o-colorido.jpg',
+      },
+      {
+        id: 45,
+        title: 'avatar 4',
+        url: 'https://img.favpng.com/15/19/18/computer-icons-ninja-favicon-iconfinder-avatar-png-favpng-Jku9r1TGGY4Ha6BLUXz1eL3DQ.jpg',
+      },
     ],
   },
   mutations: {
@@ -21,6 +38,9 @@ export default new Vuex.Store({
     },
     removeAgeMutation(state, age) {
       state.edad -= age;
+    },
+    updateUserAvatarMutation(state, userAvatar) {
+      state.userAvatar = userAvatar;
     },
   },
 });
